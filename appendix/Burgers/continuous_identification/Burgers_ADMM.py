5410adfdd52a458069079bcb27fe24f902e086ae
Jonathan <jonathan.wittmer@utexas.edu>
2019-08-11 22:51:50 -0500

    ADMM added

--- appendix/Burgers/continuous_identification/Burgers_ADMM.py
+++ appendix/Burgers/continuous_identification/Burgers_ADMM.py
@@ -12,8 +12,11 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 import matplotlib.gridspec as gridspec
 import time
 
+import os
 import sys
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 sys.path.insert(0, '../../Utilities/')
+
 
 np.random.seed(1234)
 tf.set_random_seed(1234)
@@ -21,56 +24,101 @@ tf.set_random_seed(1234)
 
 class PhysicsInformedNN:
     # Initialize the class
-    def __init__(self, X, u, layers, lb, ub):
+    def __init__(self, X_data, u_data, layers, lb, ub, X_phys=0):
         
         self.lb = lb
         self.ub = ub
         
-        self.x = X[:, 0:1]
-        self.t = X[:, 1:2]
-        self.u = u
-        
+        # break up data
+        self.x_data = X_data[:, 0:1]
+        self.t_data = X_data[:, 1:2]
+        self.u = u_data
+
+        # break up physics terms. If no physics terms are provides, default to using the data
+        if X_phys == 0:
+            self.x_phys = X_data[:, 0:1]
+            self.t_phys = X_data[:, 1:2]
+        else:
+            self.x_phys = X_phys[:, 0:1]
+            self.t_phys = X_phys[:, 1:2]
+            
+        # Save dimensions
+        self.N_u = self.x_data.shape[0]
+        self.N_r = self.x_phys.shape[0]
+
+        # list of number of nodes in each layer - first is input layer
         self.layers = layers
         
-        # Initialize NNs
+        # Initialize training variables
         self.weights, self.biases = self.initialize_NN(layers)
         
         self.config = tf.ConfigProto(allow_soft_placement=True,
                                      log_device_placement=True,
                                      intra_op_parallelism_threads=1,
                                      inter_op_parallelism_threads=2)
-
+        
         # tf placeholders and graph
         self.sess = tf.Session(config=self.config)
         
-        # Initialize parameters
-        self.lambda_1 = tf.Variable([0.0], dtype=tf.float32)
-        self.lambda_2 = tf.Variable([-6.0], dtype=tf.float32)
+        # Initialize PDE parameters
+        self.lambda_1 = tf.Variable([1.0], dtype=tf.float32)
+        self.lambda_2 = tf.Variable([-0.0031831], dtype=tf.float32)
         
-        self.x_tf = tf.placeholder(tf.float32, shape=[None, self.x.shape[1]])
-        self.t_tf = tf.placeholder(tf.float32, shape=[None, self.t.shape[1]])
+        # placeholders for training data
+        self.x_data_tf = tf.placeholder(tf.float32, shape=[None, self.x_data.shape[1]])
+        self.t_data_tf = tf.placeholder(tf.float32, shape=[None, self.t_data.shape[1]])
         self.u_tf = tf.placeholder(tf.float32, shape=[None, self.u.shape[1]])
                 
-        self.u_pred = self.net_u(self.x_tf, self.t_tf)
-        self.f_pred = self.net_f(self.x_tf, self.t_tf)
+        # placeholders for training collocation points
+        self.x_phys_tf = tf.placeholder(tf.float32, shape=[None, self.x_phys.shape[1]])
+        self.t_phys_tf = tf.placeholder(tf.float32, shape=[None, self.t_phys.shape[1]])
+
+        # evaluate outputs of network
+        self.u_pred = self.net_u(self.x_data_tf, self.t_data_tf)
+        self.f_pred = self.net_f(self.x_phys_tf, self.t_phys_tf)
+
+        # initialize the ADMM variables - just lists here as tensorflow isn't training them
+        self.z = tf.Variable(tf.ones([self.N_r, 1]), dtype=tf.float32)
+        self.gamma = tf.Variable(tf.ones([self.N_r, 1]), dtype=tf.float32)
+        self.rho = tf.constant(10.0)
+        self.c_gamma = 1 / (self.rho * self.N_r)
+        #self.dummy_z = np.zeros((self.N_r, 1))
+        self.zeros = tf.zeros((self.N_r, 1))
+        self.ones  = tf.ones((self.N_r, 1))
+
+        # ADMM loss term for training the weights - use backprop on this
+        self.loss = 1 / self.N_u * tf.pow(tf.norm(self.u - self.u_pred, 2), 2) + \
+                    tf.pow(tf.matmul(tf.transpose(self.gamma), self.f_pred), 2) + \
+                    self.rho / 2 * tf.pow(tf.norm(self.f_pred - self.z, 2), 2)
         
-        # first term: data misfit, second term: PDE residual
-        self.loss = tf.reduce_mean(tf.square(self.u_tf - self.u_pred)) + \
-                    tf.reduce_mean(tf.abs(self.f_pred))
+        self.gamma_update = self.gamma.assign(self.gamma + self.rho * (self.f_pred - self.z))
+        self.z_update = self.z.assign(self.compute_z())
         
+        self.admm_misfit = tf.reduce_mean(tf.abs(self.f_pred - self.z))
+
+        '''
         self.optimizer = tf.contrib.opt.ScipyOptimizerInterface(self.loss,
                                                                 method='L-BFGS-B',
+                                                                var_list=[self.weights, self.biases],
                                                                 options={'maxiter': 50000,
                                                                            'maxfun': 50000,
                                                                            'maxcor': 50,
                                                                            'maxls': 50,
                                                                            'ftol': 1.0 * np.finfo(float).eps})
-    
+        ''' 
         self.optimizer_Adam = tf.train.AdamOptimizer()
-        self.train_op_Adam = self.optimizer_Adam.minimize(self.loss)
+        self.train_op_Adam = self.optimizer_Adam.minimize(self.loss, 
+                                                          var_list=[self.weights, 
+                                                                    self.biases])
+                                                                    #self.lambda_1,
+                                                                    #self.lambda_2])
         
         init = tf.global_variables_initializer()
         self.sess.run(init)
+        
+        # assign the real initial value of z = r(w) 
+        self.sess.run(self.z.assign(self.f_pred), 
+                      feed_dict={self.x_phys_tf: self.x_phys, self.t_phys_tf: self.t_phys})
 
     def initialize_NN(self, layers):
         weights = []
@@ -108,7 +156,8 @@ class PhysicsInformedNN:
     
     def net_f(self, x, t):
         lambda_1 = self.lambda_1
-        lambda_2 = tf.exp(self.lambda_2)
+        #lambda_2 = tf.exp(self.lambda_2)
+        lambda_2 = self.lambda_2
         u = self.net_u(x, t)
         u_t = tf.gradients(u, t)[0]
         u_x = tf.gradients(u, x)[0]
@@ -118,33 +167,51 @@ class PhysicsInformedNN:
         return f
     
     def callback(self, loss, lambda_1, lambda_2):
-        print('Loss: %e, l1: %.5f, l2: %.5f' % (loss, lambda_1, np.exp(lambda_2)))
+        print('Loss: %e, l1: %.5f, l2: %.5f' % (loss, lambda_1, lambda_2))
         
+    def compute_z(self):
+        val   = self.f_pred + self.gamma / self.rho
+
+        # annoying digital logic workaround to implement conditional.
+        # construct vectors of 1's and 0's that we can multiply
+        # by the proper value and sum together
+        cond1 = tf.where(tf.greater(val, self.c_gamma), self.ones, self.zeros)
+        cond3 = tf.where(tf.less(val, -1.0 * self.c_gamma), self.ones, self.zeros)
+        # cond2 is not needed since the complement of the intersection
+        # of (cond1 and cond3) is cond2 and already assigned to 0
+
+        dummy_z = cond1 * (val - self.c_gamma) + cond3 * (val + self.c_gamma)
+        
+        return dummy_z
+
     def train(self, nIter):
-        tf_dict = {self.x_tf: self.x, self.t_tf: self.t, self.u_tf: self.u}
+        tf_dict = {self.x_data_tf: self.x_data, self.t_data_tf: self.t_data, self.u_tf: self.u, 
+                   self.x_phys_tf: self.x_phys, self.t_phys_tf: self.t_phys}
         
         start_time = time.time()
         for it in range(nIter):
             self.sess.run(self.train_op_Adam, tf_dict)
+            self.sess.run(self.z_update, tf_dict)
+            self.sess.run(self.gamma_update, tf_dict)
             
             # Print
             if it % 100 == 0:
                 elapsed = time.time() - start_time
                 loss_value = self.sess.run(self.loss, tf_dict)
-                lambda_1_value = self.sess.run(self.lambda_1)
-                lambda_2_value = np.exp(self.sess.run(self.lambda_2))
-                print('It: %d, Loss: %.3e, Lambda_1: %.3f, Lambda_2: %.6f, Time: %.2f' %
-                      (it, loss_value, lambda_1_value, lambda_2_value, elapsed))
+                r_z = self.sess.run(self.admm_misfit, tf_dict)
+                print('It: %d, Loss: %.3e, r(w) - z: %.3f ,Time: %.2f' %
+                      (it, loss_value, r_z, elapsed))
                 start_time = time.time()
-        
+        '''
         self.optimizer.minimize(self.sess,
                                 feed_dict=tf_dict,
                                 fetches=[self.loss, self.lambda_1, self.lambda_2],
                                 loss_callback=self.callback)
-        
+        '''
     def predict(self, X_star):
         
-        tf_dict = {self.x_tf: X_star[:, 0:1], self.t_tf: X_star[:, 1:2]}
+        tf_dict = {self.x_data_tf: X_star[:, 0:1], self.t_data_tf: X_star[:, 1:2],
+                   self.x_phys_tf: X_star[:, 0:1], self.t_phys_tf: X_star[:, 1:2]}
         
         u_star = self.sess.run(self.u_pred, tf_dict)
         f_star = self.sess.run(self.f_pred, tf_dict)
@@ -184,7 +251,7 @@ if __name__ == "__main__":
     u_train = u_star[idx, :]
     
     model = PhysicsInformedNN(X_u_train, u_train, layers, lb, ub)
-    model.train(0)
+    model.train(10000)
     
     u_pred, f_pred = model.predict(X_star)
             
@@ -306,5 +373,6 @@ if __name__ == "__main__":
     s5 = r'\end{tabular}$'
     s = s1 + s2 + s3 + s4 + s5
     ax.text(0.1, 0.1, s)
-        
-    plt.show()
+    
+    plt.savefig('figures/ADMM_L1_rho_10.png', dpi=300)
+    #plt.show()
