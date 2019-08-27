4cc81960c61488804f9e7b641f89f2acb52197d3
HwanGoh <Hwan.Goh@gmail.com>
2019-08-27 17:51:30 -0500

    Euler's data generated, the small blips are a bit worring though

--- Eulers/Data/Eulers_data_generator.m
+++ Eulers/Data/Eulers_data_generator.m
@@ -11,19 +11,40 @@ addpath Grid/Other
 
 cpt = cputime;
 
-%=== Compute Solution ===%
+%% =======================================================================%
+%                             Compute Solution
+%=========================================================================%
 EulerDriver1D   
 
-%=== Removing Duplicate Unwanted Spatial Data ===%
-spatial_indices_to_be_removed = [2,3,5,6];
-x(spatial_indices_to_be_removed,:) = [];
+%% =======================================================================%
+%                                Outputs
+%=========================================================================%
+rhosol = rho_tstep;
+usol = u_tstep;
+Enersol = Ener_tstep;
+t = time_steps_storage';
+
+%=== Removing Duplicate Spatial Data ===%
 x = x(:);
 [~, ind] = unique(x); % identify indices for unique entries of x
-duplicate_ind = setdiff(1:size(x, 1), ind); % indentify indices for duplicate indices of x
+duplicate_ind = setdiff(1:size(x, 1), ind); % identify indices for duplicate indices of x
 x(duplicate_ind) = [];
-rho_tstep(duplicate_ind,:) = [];
-u_tstep(duplicate_ind,:) = [];
-Ener_tstep(duplicate_ind,:) = [];
+rhosol(duplicate_ind,:) = [];
+usol(duplicate_ind,:) = [];
+Enersol(duplicate_ind,:) = [];
+
+%=== Removing Unwanted Spatial Data ===%
+vector_counter = 1;
+for i = 1:length(x)
+    if mod(i,5) ~= 0
+        spatial_indices_to_be_removed(vector_counter) = i;
+        vector_counter = vector_counter + 1;
+    end
+end
+x(spatial_indices_to_be_removed,:) = [];
+rhosol(spatial_indices_to_be_removed,:) = [];
+usol(spatial_indices_to_be_removed,:) = [];
+Enersol(spatial_indices_to_be_removed,:) = [];
 
 %=== Removing Unwanted Temporal Data ===%
 vector_counter = 1;
@@ -33,47 +54,44 @@ for i = 1:length(time_steps_storage)
         vector_counter = vector_counter + 1;
     end
 end
-t = time_steps_storage';
 t(temporal_indices_to_be_removed) = [];
-
-%=== Outputs ===%
-rhosol = rho_tstep(:);
-usol = u_tstep(:);
-Enersol = Ener_tstep(:);
+rhosol(:,temporal_indices_to_be_removed) = [];
+usol(:,temporal_indices_to_be_removed) = [];
+Enersol(:,temporal_indices_to_be_removed) = [];
 
 save('Abgrall_eulers','x','t','rhosol','usol','Enersol')
 
-clear rho_tstep u_tstep Ener_tstep times_steps_storage
+%% =======================================================================%
+%                               Plotting
+%=========================================================================%
+Fig_rhosol = figure;
+title('rho')
 
-%=== Plotting ===%
-% Fig_rhosol = figure;
-% title('rho')
-% 
-% Fig_usol = figure;
-% title('u')
-% 
-% Fig_Enersol = figure;
-% title('Ener')
-% 
-% for tstep=1:length(t)
-%     figure(Fig_rhosol);
-%     plot(x,rhosol(:,tstep));
-%     
-%     pause(0.01)
-% end
-% 
-% for tstep=1:length(t)
-%     figure(Fig_usol)
-%     plot(x,usol(:,tstep))
-%     title('u')
-%     
-%     pause(0.01)
-% end
-% 
-% for tstep=1:length(t)
-%     figure(Fig_Enersol)
-%     plot(x,Enersol(:,tstep));
-%     title('Ener')
-%     
-%     pause(0.01)
-% end
+Fig_usol = figure;
+title('u')
+
+Fig_Enersol = figure;
+title('Ener')
+
+for tstep=1:length(t)
+    figure(Fig_rhosol);
+    plot(x,rhosol(:,tstep));
+    
+    pause(0.1)
+end
+
+for tstep=1:length(t)
+    figure(Fig_usol)
+    plot(x,usol(:,tstep))
+    title('u')
+    
+    pause(0.1)
+end
+
+for tstep=1:length(t)
+    figure(Fig_Enersol)
+    plot(x,Enersol(:,tstep));
+    title('Ener')
+    
+    pause(0.1)
+end
